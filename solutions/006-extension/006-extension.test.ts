d633493a311de0bded0cebfc957f9b158e82cf73
Erty Seidohl <erty@seidohl.com>
2025-04-05 00:23:46 -0400

    Add 006-extension

---
+++ solutions/006-extension/006-extension.test.ts
@@ -0,0 +1,43 @@
+/*
+006 - Extension
+
+Types and interfaces can be extended, which allows you to create new types and
+interfaces based on existing ones.
+
+Create a new interface B that extends A, and a new type U that extends T.
+*/
+
+import { A, T } from "./006-types";
+
+interface B extends A {
+  b: number;
+}
+
+type U = T & { y: number };
+
+/* DO NOT EDIT BELOW THIS LINE */
+
+function test_1(input: A): B {
+  return {
+    a: input.a,
+    b: input.a,
+  };
+}
+
+function test_2(input: T): U {
+  return {
+    x: input.x,
+    y: input.x,
+  };
+}
+
+import { test } from "node:test";
+import { strict as assert } from "node:assert";
+
+test("test_1 copies the value in input.a in an object as properties a and b", async (t) => {
+  assert.deepStrictEqual(test_1({ a: 42 }), { a: 42, b: 42 });
+});
+
+test("test_2 copies the value in input.x in an object as properties x and y", async (t) => {
+  assert.deepStrictEqual(test_2({ x: 42 }), { x: 42, y: 42 });
+});
---
+++ test/006-extension/006-types.ts
@@ -0,0 +1,14 @@
+/*
+Types for the 006-extension tests.
+
+Do not modify this file. Fix the failing tests by changing the code in
+006-extension.test.ts.
+*/
+
+export interface A {
+  a: number;
+}
+
+export type T = {
+  x: number;
+};
