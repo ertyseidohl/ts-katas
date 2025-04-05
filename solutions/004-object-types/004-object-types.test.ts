30452a5570a8608cc0f9abd970afda63b0fa6a8c
Erty Seidohl <erty@seidohl.com>
2025-04-04 21:51:56 -0400

    Fix and extend 004-object-types

--- solutions/004-object-types/004-object-types.test.ts
+++ solutions/004-object-types/004-object-types.test.ts
@@ -1,15 +1,28 @@
 /*
-003 - Async
+004 - Object types
 
-Async functions should return a Promise. Fix the function below so the tests
-pass.
+You can define object value types inline, or use an interface. Fix the functions
+below so that the tests pass.
 */
 
-function main(): { n: number; s: string; b: boolean } {
+function test_1(): { n: number; s: string; b: boolean } {
   return {
     n: 42,
-    s: "Hello",
+    s: "7",
     b: true,
+  };
+}
+
+// Do not modify the interface below. Modify the code in test_2 instead.
+interface I {
+  a: number;
+  b: number;
+}
+
+function test_2(): I {
+  return {
+    a: 4,
+    b: 4,
   };
 }
 
@@ -18,8 +31,14 @@ function main(): { n: number; s: string; b: boolean } {
 import { test } from "node:test";
 import { strict as assert } from "node:assert";
 
-test("main() returns an object with a number, string, and boolean", async (t) => {
-  assert.equal(typeof main().n, "number");
-  assert.equal(typeof main().s, "string");
-  assert.equal(typeof main().b, "boolean");
+test("test_1() returns an object with a number, string, and boolean", async (t) => {
+  assert.equal(typeof test_1().n, "number");
+  assert.equal(typeof test_1().s, "string");
+  assert.equal(typeof test_1().b, "boolean");
+});
+
+test("test_2() returns an object matching the interface I", async (t) => {
+  assert.equal(Object.keys(test_2()).sort(), ["a", "b"]);
+  assert.equal(typeof test_2().a, "number");
+  assert.equal(typeof test_2().b, "number");
 });
